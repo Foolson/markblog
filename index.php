ba63afd215ea9b6ed7b795e15ba8ba5e8b925961
Foolson <johan.olsson.91@gmail.com>
2016-08-25 19:34:42 +0200

    moved the functions to a separate library

--- index.php
+++ index.php
@@ -11,66 +11,15 @@
     <div class="container">
       <h1>Blog</h1>
 <?php
-include 'lib/Parsedown.php';
-include 'lib/Spyc.php';
-
-# Function to get all files in directory and ignore '..' and '.'
-function getFiles($dir){
-  return array_diff(scandir($dir), array('..', '.'));
-}
-
-# Function to convert post title to a slug
-function toSlug($string){
-  return strtolower(trim(preg_replace('/[^A-Za-z0-9-åäöÅÄÖ]+/', '-', $string)));
-}
-
-# Function to gather all posts with their metadata
-function getPosts($files, $folder){
-  $posts = array();
-  foreach($files as $post){
-    $filename = "$folder/$post";
-    $handle = fopen($filename, "r");
-    $contents = fread($handle, filesize($filename));
-    fclose($handle);
-    $subject = $contents;
-    $pattern = '/^---\n([\s\S]+)\n---\n/';
-    preg_match($pattern, $subject, $matches);
-    $postMeta = spyc_load($matches[1]);
-    $post = $postMeta;
-    $postBody =  preg_replace('/^---\n[\s\S]+\n---\n/', '', $contents);
-    $post['slug'] = toSlug($post['title']);
-    $post['body'] = $postBody;
-    array_push($posts, $post);
+  require_once __DIR__.'/resources/library/markblog.php';
+  # Get all posts and print them out in HTML
+  foreach(sortPosts(getPosts(getFiles(__DIR__.'/resources/posts/'), __DIR__.'/resources/posts/')) as $post){
+    echo('<a href="post.php?slug='.$post['slug'].'"><h2>'.$post['title']."</h2></a>\n");
+    echo("<p>".$post['author']." - ".$post['datetime']."</p>\n");
+    echo(convertMarkdown($post['body']));
   }
-  return $posts;
-}
-
-# Function to sort all posts after datetime
-function sortPosts($posts) {
-  function date_compare($a, $b)
-  {
-      $t1 = strtotime($a['datetime']);
-      $t2 = strtotime($b['datetime']);
-      return $t2 - $t1;
-  }
-  usort($posts, 'date_compare');
-  return $posts;
-}
-
-# Function to convert markdown to HTML
-function convertMarkdown($body){
-  $Parsedown = new Parsedown();
-  return $Parsedown->text($body);
-}
-
-# Get all posts and print them out in HTML
-foreach(sortPosts(getPosts(getFiles('post'), 'post/')) as $post){
-  echo('   <a href="post.php?slug='.$post['slug'].'"><h2>'.$post['title']."</h2></a>\n");
-  echo("    <p>".$post['author']." - ".$post['datetime']."</p>\n");
-  echo(convertMarkdown($post['body']));
-}
 ?>
-    </div>
+      </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
