bbf59cef7710d308a4d480d839b7684bb02b910e
Foolson <johan.olsson.91@gmail.com>
2016-08-25 19:43:14 +0200

    moved header and footer to templates

--- index.php
+++ index.php
@@ -1,27 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Blog</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <h1>Blog</h1>
+<?php $workingDir = __DIR__; ?>
+
+<?php require_once "$workingDir/resources/templates/header.php"; ?>
+
 <?php
-  require_once __DIR__.'/resources/library/markblog.php';
+  require_once "$workingDir/resources/library/markblog.php";
   # Get all posts and print them out in HTML
-  foreach(sortPosts(getPosts(getFiles(__DIR__.'/resources/posts/'), __DIR__.'/resources/posts/')) as $post){
+  foreach(sortPosts(getPosts(getFiles("$workingDir/resources/posts/"), "$workingDir/resources/posts/")) as $post){
     echo('<a href="post.php?slug='.$post['slug'].'"><h2>'.$post['title']."</h2></a>\n");
     echo("<p>".$post['author']." - ".$post['datetime']."</p>\n");
     echo(convertMarkdown($post['body']));
   }
 ?>
-      </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
-  </body>
-</html>
+
+<?php require_once "$workingDir/resources/templates/footer.php"; ?>
