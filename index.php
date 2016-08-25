3bf9fcaae280388bb58545ca84dcd9a410e0fade
Foolson <johan.olsson.91@gmail.com>
2016-08-25 20:13:58 +0200

    Changed the style of the author/datetime thing

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   # Get all posts and print them out in HTML
   foreach(sortPosts(getPosts(getFiles("$workingDir/resources/posts/"), "$workingDir/resources/posts/")) as $post){
     echo('<a href="post.php?slug='.$post['slug'].'"><h2>'.$post['title']."</h2></a>\n");
-    echo("<p>".$post['author']." - ".$post['datetime']."</p>\n");
+    echo('<p class="text-muted"><em>'.$post['author']." - ".$post['datetime']."</em></p>\n");
     echo(convertMarkdown($post['body']));
   }
 ?>
