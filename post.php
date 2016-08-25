51f79fe6cfd2bc1c9d2f59eeef6c3f4d93cd29d1
Foolson <johan.olsson.91@gmail.com>
2016-08-25 20:13:25 +0200

    Added some newline characters and changed the style of the author/datetime thing

--- post.php
+++ post.php
@@ -7,12 +7,12 @@
   # Get all posts and print them out in HTML
   foreach(sortPosts(getPosts(getFiles("$workingDir/resources/posts/"), "$workingDir/resources/posts/")) as $post){
     if($post['slug'] == $_GET['slug']){
-      echo('<ol class="breadcrumb">');
-      echo('<li class="breadcrumb-item"><a href="index.php">Start</a></li>');
-      echo('<li class="breadcrumb-item active">'.$post['title'].'</li>');
-      echo('</ol>');
+      echo('<ol class="breadcrumb">'."\n");
+      echo('<li class="breadcrumb-item"><a href="/">Start</a></li>'."\n");
+      echo('<li class="breadcrumb-item active">'.$post['title']."</li>\n");
+      echo("</ol>\n");
       echo('<h2>'.$post['title']."</h2>\n");
-      echo("<p>".$post['author']." - ".$post['datetime']."</p>\n");
+      echo('<p class="text-muted"><em>'.$post['author']." - ".$post['datetime']."</em></p>\n");
       echo(convertMarkdown($post['body']));
     }
   }
