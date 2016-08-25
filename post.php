c1c2084822dde6c91a64fe01a0305192cdfc07a1
Foolson <johan.olsson.91@gmail.com>
2016-08-25 19:57:57 +0200

    Added breadcrumb

--- post.php
+++ post.php
@@ -7,6 +7,10 @@
   # Get all posts and print them out in HTML
   foreach(sortPosts(getPosts(getFiles("$workingDir/resources/posts/"), "$workingDir/resources/posts/")) as $post){
     if($post['slug'] == $_GET['slug']){
+      echo('<ol class="breadcrumb">');
+      echo('<li class="breadcrumb-item"><a href="index.php">Start</a></li>');
+      echo('<li class="breadcrumb-item active">'.$post['title'].'</li>');
+      echo('</ol>');
       echo('<h2>'.$post['title']."</h2>\n");
       echo("<p>".$post['author']." - ".$post['datetime']."</p>\n");
       echo(convertMarkdown($post['body']));
