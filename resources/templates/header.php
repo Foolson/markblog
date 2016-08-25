1d91438a11b92d7e8650c3735c97c4eaf7c296b4
Foolson <johan.olsson.91@gmail.com>
2016-08-25 19:44:00 +0200

    Added header and footer contet to templates

---
+++ resources/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>Blog</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+<h1>Blog</h1>
